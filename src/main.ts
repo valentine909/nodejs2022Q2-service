f0748aa471292d2f56c4576156ed6157f55a1869
Valentine <valentine.nazarov@gmail.com>
2022-07-12 15:08:48 +0300

    feat: add env port support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
