901cd828aee5b93c3832ba26774237b7dcfbd47c
Valentine <valentine.nazarov@gmail.com>
2022-07-21 16:23:02 +0300

    refactor: make changes to config

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,15 +1,15 @@
 import 'dotenv/config';
 import { DataSourceOptions } from 'typeorm';
 
-export = {
+export const configService = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST as string,
   port: parseInt(process.env.POSTGRES_PORT as string, 10) as number,
   username: process.env.POSTGRES_USER as string,
   password: process.env.POSTGRES_PASSWORD as string,
   database: process.env.POSTGRES_DATABASE as string,
-  synchronize: false,
-  entities: ['src/**/**.entity.ts'],
+  synchronize: true,
+  entities: ['dist/user/entities/user.entity.js'],
   migrations: ['src/database/migrations/*.ts'],
   cli: {
     migrationsDir: 'src/database/migrations',
